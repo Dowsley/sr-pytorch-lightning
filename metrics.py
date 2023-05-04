11db33486283e5ce344286f1e6255d2c975451d6
Felipe Dowsley <dowsley@fb.com>
2023-05-04 13:41:05 -0300

    img test

--- metrics.py
+++ metrics.py
@@ -13,8 +13,10 @@ if __name__ == '__main__':
     parser.add_argument('--predict_datasets', type=str) 
 
     args = parser.parse_args()
-
-    length = len(listdir(f'{args.datasets_dir}/{args.predict_datasets}'))
+    original_dir = f'{args.datasets_dir}/{args.predict_datasets}'
+    sr_dir = f'{args.default_root_dir}/{args.predict_datasets}'
+    print(original_dir, sr_dir)
+    length = len(listdir(original_dir))
 
     ssim_scores = []
     psnr_scores = []
